fe2c8c03951ba21a8ee9e93a9980b6c493326e2e
kitsw1 <91342621+kitsw1@users.noreply.github.com>
2021-09-24 23:32:56 +0530

    Add files via upload

---
+++ cssverticalmenu.css
@@ -0,0 +1,100 @@
+.glossymenu, .glossymenu li ul, .glossymenu li ul ul{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 130px; /*WIDTH OF MAIN MENU ITEMS*/
+    z-index: 1 !important ;
+    }
+    
+    .glossymenu li{
+    position: relative;
+    }
+    
+    .glossymenu li a{
+        background: white url(glossyback.html) repeat-x bottom left;
+        font: bold 13px Arial, Helvetica, sans-serif;
+        color: #000;
+        display: block;
+        width: auto;
+        padding: 2px 0;
+        text-decoration: none;
+        border-bottom-color:#209dec;
+        border-bottom-width:1px;
+        border-bottom-style:solid;
+    }
+    
+    .glossymenu li a:hover{
+        background: white url(glossyback.html) repeat-x bottom left;
+        font: bold 13px Arial, Helvetica, sans-serif;
+        color:#3399ff;
+        display: block;
+        width: 130px;
+        padding: 2px 0;
+        text-decoration: none;
+        border-bottom-color:#ff6600;
+        border-bottom-width:1px;
+        border-bottom-style:solid;
+        z-index: 1 !important;
+    }
+    .glossymenu li ul{ /*SUB MENU STYLE*/
+    position: absolute;
+    width: 130px; /*WIDTH OF SUB MENU ITEMS*/
+    left: 0;
+    top: 0;
+    display: none;
+    }
+    
+    .glossymenu li ul ul{ /*SUB MENU STYLE*/
+    position: absolute;
+    width: 130px; /*WIDTH OF SUB MENU ITEMS*/
+    left: 100;
+    top: 0;
+    display: none;
+    }
+    
+    .glossymenu li ul li{
+    float: left;
+    margin-left:5px;
+    }
+    
+    .glossymenu li ul ul li{
+    float: left;
+    padding-left:130px;
+    
+    }
+    
+    .glossymenu li ul a{
+    width: 130px; /*WIDTH OF SUB MENU ITEMS - 10px padding-left for A elements */
+    
+    
+    }
+    .glossymenu li ul ah:hover{
+    
+    padding-right:10px;
+    z-index: 1 !important ;
+    
+    }
+    .glossymenu li ul ul a{
+    width: 130px; /*WIDTH OF SUB MENU ITEMS - 10px padding-left for A elements */
+    
+    }
+    
+    .glossymenu .arrowdiv{
+    position: absolute;
+    right: 2px;
+    background: transparent url(arrow.gif) no-repeat center right;
+    }
+    
+    .glossymenu li a:visited, .glossymenu li a:active{
+    color: #000;
+    }
+    
+    .glossymenu li a:hover{
+    background-image: url(glossyback2.html);
+    z-index: 1 !important ;
+    }
+    
+    /* Holly Hack for IE \*/
+    * html .glossymenu li { float: left; height: 1%; }
+    * html .glossymenu li a { height: 1%; }
+    /* End */
